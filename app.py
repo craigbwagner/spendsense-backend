c409c5223e2bf929a3bc76b5fe92ae0118c115a7
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-07-31 09:35:15 -0400

    init repo

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+
+def get_db_connection():
+    connection = psycopg2.connect(
+        host="localhost",
+        database="flask_auth_db",
+        user=os.getenv("POSTGRES_USERNAME"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+    )
+    return connection
