f700787256e4ffbd79578c10fe7b52abf80b24a7
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-07-31 16:04:54 -0400

    add update_settings route

--- settings_blueprint.py
+++ settings_blueprint.py
@@ -34,3 +34,39 @@ def create_settings():
         return jsonify({"expense": created_expense}), 201
     except Exception as e:
         return jsonify({"Error": str(e)}), 400
+
+
+@settings_blueprint.route("/settings", methods=["PUT"])
+@token_required
+def update_settings():
+    try:
+        updated_settings_data = request.json
+        connection = get_db_connection()
+        cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        user_id = g.user["id"]
+        cursor.execute("SELECT * FROM settings WHERE user_id = %s", (user_id,))
+        settings_to_update = cursor.fetchone()
+        if settings_to_update is None:
+            return jsonify({"error": "User settings not found"}), 404
+        connection.commit()
+        if settings_to_update["user_id"] is not user_id:
+            return ({"error": "Unauthorized"}), 401
+        cursor.execute(
+            """
+                UPDATE settings SET monthly_income = %s, monthly_budget = %s, weekly_budget = %s, savings_goal = %s WHERE user_id = %s RETURNING *
+            """,
+            (
+                updated_settings_data["monthly_income"],
+                updated_settings_data["monthly_budget"],
+                updated_settings_data["weekly_budget"],
+                updated_settings_data["savings_goal"],
+                user_id,
+            ),
+        )
+        updated_settings = cursor.fetchone()
+        print(updated_settings)
+        connection.commit()
+        connection.close()
+        return jsonify({"settings": updated_settings}), 200
+    except Exception as e:
+        return jsonify({"Error": str(e)}), 400
