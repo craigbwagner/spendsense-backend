deb93824649643b3145247cedae0f17e34d36c14
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-01 11:09:41 -0400

    add create_category_budgets that initializes an empty budget upon user signup

--- src/blueprints/category_budgets_blueprint.py
+++ src/blueprints/category_budgets_blueprint.py
@@ -10,25 +10,54 @@ def create_category_budgets(user_id):
     try:
         connection = get_db_connection()
         cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-        monthly_income = 0
-        monthly_budget = 0
-        savings_goal = 0
+        housing_budget = 0
+        transportation_budget = 0
+        food_groceries_budget = 0
+        utilities_budget = 0
+        clothing_budget = 0
+        medical_budget = 0
+        insurance_budget = 0
+        personal_budget = 0
+        education_budget = 0
+        entertainment_budget = 0
+        other_budget = 0
         cursor.execute(
             """
-                            INSERT INTO settings (monthly_income, monthly_budget, savings_goal, user_id)
-                            VALUES (%s, %s,%s,%s,%s)
-                            RETURNING *
-                        """,
+                INSERT INTO category_budgets (
+                    user_id,
+                    housing,
+                    transportation,
+                    food_groceries,
+                    utilities,
+                    clothing,
+                    medical,
+                    insurance,
+                    personal,
+                    education,
+                    entertainment,
+                    other)
+                    VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+                    RETURNING *
+            """,
             (
-                monthly_income,
-                monthly_budget,
-                savings_goal,
                 (user_id,),
+                housing_budget,
+                transportation_budget,
+                food_groceries_budget,
+                utilities_budget,
+                clothing_budget,
+                medical_budget,
+                insurance_budget,
+                personal_budget,
+                education_budget,
+                entertainment_budget,
+                other_budget,
             ),
         )
-        created_settings = cursor.fetchone()
+        created_category_budgets = cursor.fetchone()
         connection.commit()
-        connection.close()
-        return jsonify({"expense": created_settings}), 201
+        return jsonify({"category_budgets": created_category_budgets}), 201
     except Exception as e:
         return jsonify({"Error": str(e)}), 400
+    finally:
+        connection.close()
